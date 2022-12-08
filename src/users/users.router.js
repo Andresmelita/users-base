943165a4313b8ad6a0808d243dd740da1c8d08d3
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2022-12-08 16:09:43 -0300

    protected routes

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -6,10 +6,10 @@ const userServices = require('./users.services')
 
 //? /api/v1/users
 router.get("/", passportJWT.authenticate('jwt', {session: false}), userServices.getAllUsers) 
-router.post("/", userServices.postUser)
+router.post("/", userServices.postUser) //*Agregar usuarios no requerirá autenticación
 
-router.get("/:id", userServices.getUserById)
-router.patch('/:id', userServices.patchUser)
-router.delete('/:id', userServices.deleteUser)
+router.get("/:id", passportJWT.authenticate('jwt', {session: false}),userServices.getUserById)
+router.patch('/:id', passportJWT.authenticate('jwt', {session: false}), userServices.patchUser)
+router.delete('/:id', passportJWT.authenticate('jwt', {session: false}),userServices.deleteUser)
 
 module.exports = router
